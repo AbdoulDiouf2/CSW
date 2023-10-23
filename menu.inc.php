665326daa4960682ff2db22cb93f1b54def5b01b
Abdoul Ahad Mbacké DIOUF <abdoul.diouf2@groupe-esigelec.org>
2023-10-23 22:52:24 +0200

    Création de la page About us Ajout du bouton "à propos" sur le header Ajout du logo sur le header

--- menu.inc.php
+++ menu.inc.php
@@ -1,6 +1,7 @@
 <nav class="navbar navbar-expand-md bg-dark border-bottom border-body" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Esigelec</a>
+    <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="Logo du site"></a>
+    <a class="navbar-brand" href="index.php">Esigelec</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,6 +15,9 @@
         </li>
       </ul>
       <ul class="navbar-nav mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link"  href="aboutus.php">A Propos</a>
+        </li>
         <li class="nav-item">
           <a class="nav-link"  href="inscription.php">Inscription</a>
         </li>
