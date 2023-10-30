43501de4658c5e55a36353b6af433a98a417e117
Abdoul Ahad Mbacké DIOUF <abdoul.diouf2@groupe-esigelec.org>
2023-10-31 00:01:00 +0100

    Mise à jour de la Page d'accueil

--- inscription.php
+++ inscription.php
@@ -9,15 +9,15 @@
 <h1>Inscription</h1>
 <form  method="POST" action="tt_inscription.php">
     <div class="container">
-    <div class="row my-3">
-        <div class="col-md-6">
-            <label for="nom" class="form-label">Nom</label>
-            <input type="text" class="form-control " id="nom" name="nom" placeholder="Votre nom..." required>
-        </div>
-        <div class="col-md-6">
-            <label for="prenom" class="form-label">Prénom</label>
-            <input type="text" class="form-control " id="prenom" name="prenom" placeholder="Votre prénom..." required>
-        </div>
+        <div class="row my-3">
+            <div class="col-md-6">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" class="form-control " id="nom" name="nom" placeholder="Votre nom..." required>
+            </div>
+            <div class="col-md-6">
+                <label for="prenom" class="form-label">Prénom</label>
+                <input type="text" class="form-control " id="prenom" name="prenom" placeholder="Votre prénom..." required>
+            </div>
         </div>
         <div class="row">
         <div class="col-md-6">
@@ -30,7 +30,7 @@
         </div>
         </div>
         <div class="row my-3">
-        <div class="d-grid gap-2 d-md-block"><button class="btn btn-outline-primary" type="submit">Inscription</button></div>   
+            <div class="d-grid gap-2 d-md-block"><button class="btn btn-outline-primary" type="submit">Inscription</button></div>   
         </div>
     </div>
 </form>
